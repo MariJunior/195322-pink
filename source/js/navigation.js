1c3896f8b34c913b70b2a59a88461b5aeee6520a
MariJunior <marinakaluzhnaya13@gmail.com>
2018-05-12 16:31:07 +0300

    Продолжает вёрстку. Реализация открытия/закрытия меню на мобильной и планшетной версиях. Добавляет файлы будущих скриптов для карты и попапов.

---
+++ source/js/navigation.js
@@ -0,0 +1,19 @@
+var mainMenu = document.querySelector('.main-nav');
+var mainMenuToggle = document.querySelector('.page-header__nav-toggle');
+var pageHeader = document.querySelector('.page-header');
+
+pageHeader.classList.add('page-header--closed');
+mainMenu.classList.add('main-nav--closed');
+mainMenuToggle.classList.remove('page-header__nav-toggle--opened');
+
+mainMenuToggle.addEventListener('click', function() {
+  if (mainMenu.classList.contains('main-nav--closed')) {
+    mainMenu.classList.remove('main-nav--closed');
+    pageHeader.classList.remove('page-header--closed');
+    mainMenuToggle.classList.add('page-header__nav-toggle--opened');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    mainMenu.classList.add('main-nav--closed');
+    mainMenuToggle.classList.remove('page-header__nav-toggle--opened');
+  }
+});

d30bd07fb441ad316e7cff7fdf000ad05e6c2f73
MariJunior <marinakaluzhnaya13@gmail.com>
2018-05-25 20:54:50 +0300

    Изменяет структуру страниц - помещает промо-блоки в хедер для корректного поведения при переполнении навигационного меню.

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,18 +1,18 @@
 var mainMenu = document.querySelector('.main-nav');
 var mainMenuToggle = document.querySelector('.page-header__nav-toggle');
-var pageHeader = document.querySelector('.page-header');
+var pageHeader = document.querySelector('.page-header__color-wrap');
 
-pageHeader.classList.add('page-header--closed');
+pageHeader.classList.add('page-header__color-wrap--closed');
 mainMenu.classList.add('main-nav--closed');
 mainMenuToggle.classList.remove('page-header__nav-toggle--no-js');
 
 mainMenuToggle.addEventListener('click', function() {
   if (mainMenu.classList.contains('main-nav--closed')) {
     mainMenu.classList.remove('main-nav--closed');
-    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.remove('page-header__color-wrap--closed');
     mainMenuToggle.classList.add('page-header__nav-toggle--opened');
   } else {
-    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.add('page-header__color-wrap--closed');
     mainMenu.classList.add('main-nav--closed');
     mainMenuToggle.classList.remove('page-header__nav-toggle--opened');
   }
